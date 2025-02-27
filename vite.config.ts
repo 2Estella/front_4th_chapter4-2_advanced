684b987798f66de25617576743613acf816c1c53
2Estella <krsd0501@naver.com>
2025-02-27 21:06:02 +0900

    📦 : 필요 패키지 설치 및 코드 컨벤션 세팅 작업
    - Prettier, ESLint 설치 및 설정
    - husky 설정하여 커밋 전 포맷팅 되도록 설정
    - tsconfig.app.json 팀 컨벤션으로 수정
    - package.json 패키지 설치, 버전 업데이트 및 관련 스크립트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
+import path from 'node:path';
+
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
@@ -12,9 +28,9 @@ export default mergeConfig(
       environment: 'jsdom',
       setupFiles: './src/setupTests.ts',
       coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ['lcov', 'json', 'json-summary']
+        reportsDirectory: './.coverage',
+        reporter: ['lcov', 'json', 'json-summary'],
       },
     },
-  })
-)
+  }),
+);
